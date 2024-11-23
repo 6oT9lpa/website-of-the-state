0a1e644a416a304c7b9f0f1e64ac711fde39efd3
Arnetik <serglassar@gmail.com>
2024-11-23 12:03:34 +0200

    Merge branch 'main' of https://github.com/Aleksander-Spivack/majestic

--- __init__.py
+++ __init__.py
@@ -3,29 +3,10 @@ from sqlalchemy import DateTime, func
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager
 from datetime import datetime
-import uuid
-import logging
+from werkzeug.security import check_password_hash, generate_password_hash
+import logging, string, random, uuid
 
 from main import main
-
-from flask import render_template, redirect, url_for, request, flash, Blueprint, session, send_file, make_response, jsonify, abort
-from functools import wraps
-from sqlalchemy.exc import SQLAlchemyError
-from werkzeug.security import check_password_hash, generate_password_hash
-from form import FormAuditPush, FormAuthPush, Formchangepassword, Formforgetpassword1, Formforgetpassword2
-from flask_login import login_user, login_required, logout_user, current_user
-from datetime import datetime, timedelta
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, letter
-from reportlab.lib.units import mm
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from io import BytesIO
-from textwrap import wrap
-from PyPDF2 import PdfReader, PdfWriter
-import random, string, redis, json, os, re, shutil, logging, json
-from logging import handlers
-
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -34,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = False
 app.config['WTF_CSRF_ENABLED'] = True
 app.config['DEBUG'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:arnetik1@localhost:3306/site'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Qwerty123!@localhost:3306/db_majestic'
 app.register_blueprint(main)
 
 db = SQLAlchemy(app)
@@ -52,8 +33,14 @@ def get_next_id_permission():
     last_id = db.session.query(func.max(PermissionUsers.id)).scalar()
     return (last_id or 0) + 1
 
+def get_next_id_isk():
+    last_id = db.session.query(func.max(claimsStatement.id)).scalar()
+    return (last_id or 0) + 1
+
 class Users(db.Model, UserMixin):
     __tablename__ = 'users'
+
+    user_type = db.Column(db.String(10), default='user')
 
     id = db.Column(db.Integer, primary_key=True)
     discordid = db.Column(db.BigInteger, nullable=False)
@@ -68,18 +55,40 @@ class Users(db.Model, UserMixin):
     SW = db.Column(db.Integer, nullable=False, default=0)
     password = db.Column(db.String(255), nullable=False)
 
+    @property
+    def is_guest(self):
+        return False
+
     permissions = db.relationship('PermissionUsers', back_populates='user')
     custom_resolution = db.relationship('CustomResolutionTheUser', back_populates='user')
     resolution = db.relationship('ResolutionTheUser', back_populates='user')
     create_document = db.relationship('PDFDocument', back_populates='user')
     action_log = db.relationship('ActionUsers', back_populates='user')
     order = db.relationship('OrderTheUser', back_populates='user')
+    judges = db.relationship('iskdis', foreign_keys='iskdis.judge', back_populates='judge_user')
+    prosecutors = db.relationship('iskdis', foreign_keys='iskdis.prosecutor', back_populates='prosecutor_user')
+
+
+class guestUsers(db.Model, UserMixin):
+    __tablename__ = 'guest_users'
+
+    user_type = db.Column(db.String(10), default='guest')
+
+    id = db.Column(db.Integer, primary_key=True, default=get_next_id_user)
+    discord_id = db.Column(db.BigInteger, nullable=False)
+    static = db.Column(db.Integer, nullable=False, unique=True)
+    nickname = db.Column(db.String(45), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def is_guest(self):
+        return True
 
 class PermissionUsers(db.Model, UserMixin):
     __tablename__ = 'permission'
 
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, default=get_next_id_permission)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
 
     tech = db.Column(db.Boolean, default=False)
     admin = db.Column(db.Boolean, default=False)
@@ -215,32 +224,50 @@ class News(db.Model):
     textnews = db.Column(db.Text, nullable=False)
     picture = db.Column(db.String(200), nullable=True)
 
-class iskdis(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class claimsStatement(db.Model):
+    __tablename__ = 'court_claims'
+
+    id = db.Column(db.Integer, primary_key=True, default=get_next_id_isk)
     uid = db.Column(db.String(16), unique=True, default=generate_uid)
-    judge = db.Column(db.String(45), nullable=True)
+    create_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    is_archived = db.Column(db.Boolean, default=False)
+
+    reply = db.relationship('repltoisks', back_populates='current_claim')
+    district_court = db.relationship('iskdis', back_populates='current_claim', foreign_keys='iskdis.claims_id') 
+    supreme_court = db.relationship('isksup', back_populates='current_claim', foreign_keys='isksup.claims_id')  
+    court_order = db.relationship('courtOrder', back_populates='current_claim')
+
+
+class iskdis(db.Model):
+    __tablename__ = 'district_court'
+
+    id = db.Column(db.Integer, primary_key=True)
+    current_uid = db.Column(db.String(16), unique=True)
+    judge = db.Column(db.Integer, db.ForeignKey('users.id'))
     discription = db.Column(db.Text, nullable=False)
     claims = db.Column(db.PickleType, nullable=False)
     phone = db.Column(db.String(12), nullable=False)
     cardn = db.Column(db.String(15), nullable=False)
-    createdds = db.Column(db.String(100), nullable=False)
-
-    created = db.Column(db.String(45), nullable=False) #Истец
-    defendant = db.Column(db.PickleType, nullable=True) #Ответчик(и)
-    def __repr__(self):
-        return ', '.join(map(str, self.defendant))
-    prosecutor = db.Column(db.String(45), nullable=True)
-    
-    lawerc = db.Column(db.String(45), nullable=True)
-    lawerd = db.Column(db.String(45), nullable=True)
-
+    created = db.Column(db.Integer, nullable=False)
+    defendant = db.Column(db.PickleType)
+    prosecutor = db.Column(db.Integer, db.ForeignKey('users.id'))
+    lawerc = db.Column(db.String(45))
+    lawerd = db.Column(db.String(45))
     otherme = db.Column(db.PickleType, nullable=True)
-    create_at = db.Column(db.DateTime, nullable=False, default=datetime.now().strftime("%Y-%m-%d %H:%M"))
-    is_archived = db.Column(db.Boolean, default=False)
+    claims_id = db.Column(db.String(16), db.ForeignKey('court_claims.uid')) 
+    status = db.Column(db.String(15), default='Waitting')
+
+    judge_user = db.relationship('Users', foreign_keys=[judge], back_populates='judges')
+    prosecutor_user = db.relationship('Users', foreign_keys=[prosecutor], back_populates='prosecutors')
+    current_claim = db.relationship('claimsStatement', back_populates='district_court')
+
 
 class isksup(db.Model):
+    __tablename__ = 'supreme_court'
+
     id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.String(16), unique=True, default=generate_uid)
+    current_uid = db.Column(db.String(16), unique=True, default=generate_uid)
+
     judge = db.Column(db.String(45), nullable=True)
     discription = db.Column(db.Text, nullable=False)
     claims = db.Column(db.PickleType, nullable=False)
@@ -248,28 +275,49 @@ class isksup(db.Model):
     cardn = db.Column(db.String(15), nullable=False)
     createdds = db.Column(db.String(100), nullable=False)
 
-    created = db.Column(db.String(45), nullable=False) #Истец
-    defendant = db.Column(db.PickleType, nullable=True) #Ответчик(и)
+    created = db.Column(db.String(45), nullable=False)
+    defendant = db.Column(db.PickleType, nullable=True)
     prosecutor = db.Column(db.String(45), nullable=True)
-    
+
     lawerc = db.Column(db.String(45), nullable=True)
     lawerd = db.Column(db.String(45), nullable=True)
 
     otherme = db.Column(db.PickleType, nullable=True)
-    create_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    is_archived = db.Column(db.Boolean, default=False)
+
+    claims_id = db.Column(db.String(16), db.ForeignKey('court_claims.uid')) 
+
+    current_claim = db.relationship('claimsStatement', back_populates='supreme_court')
+
 
 class repltoisks(db.Model):
+    __tablename__ = 'replto_claims'
+
     id = db.Column(db.Integer, primary_key=True)
-    type_document = db.Column(db.String(60), nullable=False)
-    uid = db.Column(db.String(16), nullable=False)
-    author = db.Column(db.String(45), nullable=False)
+    current_uid = db.Column(db.String(16), db.ForeignKey('court_claims.uid'), nullable=False)
+    author_id = db.Column(db.String(45), nullable=False)
+
     replyik = db.Column(db.PickleType, nullable=False)
 
-    
+    current_claim = db.relationship('claimsStatement', back_populates='reply')
+
+class courtOrder(db.Model):
+    __tablename__ = 'court_order'
+
+    id = db.Column(db.Integer, primary_key=True)
+    current_uid = db.Column(db.String(16), db.ForeignKey('court_claims.uid'), nullable=False)
+    author_id = db.Column(db.String(6), nullable=False)
+
+    findings = db.Column(db.PickleType, nullable=False)
+    consideration = db.Column(db.PickleType, nullable=False)
+    ruling = db.Column(db.PickleType, nullable=False)
+
+    current_claim = db.relationship('claimsStatement', back_populates='court_order')
+
+
 # Создание таблиц базы данных
 with app.app_context():
     db.create_all()
+
     
     """
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -303,9 +351,17 @@ with app.app_context():
     )
     db.session.add(permission_entry)
     db.session.commit()
-    """
-
-# Функция загрузки пользователя
+    """ 
+   
+    
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(user_id)
+    user = Users.query.get(user_id) 
+    if user:
+        return user
+
+    guest = guestUsers.query.get(user_id)
+    if guest:
+        return guest
+
+    return None
